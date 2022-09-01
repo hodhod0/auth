26f5d148ee9ce88b21f735e54cf2f9a9beddb525
hodod <alameddinehodhod@gmail.com>
2022-09-01 15:48:18 +0300

    jwt

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,10 @@
 const User = require("../models/UserModel")
+const jwt = require("jsonwebtoken")
+
+const createToken = (_id)=>{
+   return jwt.sign({_id}, process.env.SERCET,{expiresIn:"3d"})
+
+}
 
 
 //login
@@ -11,7 +17,9 @@ const singupUser = async (req,res)=>{
     const {email, password} = req.body
     try {
         const user = await User.singup(email,password)
-        res.status(200).json({email,user})
+        //create token
+        const token = createToken(user._id)
+        res.status(200).json({email,token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
