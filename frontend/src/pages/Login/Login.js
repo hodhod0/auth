904d5d3ddd69183a63ec2e730a5ca3b1c72fbf57
AhmadSayess <ahmadsayess1998@gmail.com>
2022-09-05 15:27:33 +0300

    login

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
 
 export const Login = () => {
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const {login , error , isLoading} = useLogin()
   const handelSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(email , password);
+    // console.log(email , password);
+    await login(email , password)
   };
 
   return (
@@ -28,7 +31,8 @@ export const Login = () => {
         onChange={(e) => setPassword(e.target.value)}
       />
 
-      <button>Log In</button>
+      <button disabled={isLoading} onClick={''}>Log In</button>
+      {error && <div className="error"> {error} </div>}
     </form>
   );
 };
