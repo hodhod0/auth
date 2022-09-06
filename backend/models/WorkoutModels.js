d2b077aa41fd514bd8d89be6b05c79e01cd31540
hodod <alameddinehodhod@gmail.com>
2022-09-06 17:41:17 +0300

    assgning user

--- backend/models/WorkoutModels.js
+++ backend/models/WorkoutModels.js
@@ -15,6 +15,10 @@ const WorkoutSchema = new Schema(
       type: Number,
       required: true,
     },
+    user_id:{
+      type:String,
+      required:true
+    }
   },
   { timestamps: true }
 );
